d65c5740450149003b5f7271536c76474cf2bc57
adeniji-adebanjo <aeben.adebanjo@gmail.com>
2025-12-16 11:08:33 +0100

    feat: Initialize Next.js frontend application with base configuration and dependencies.

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,63 +1,60 @@
 const nextConfig = {
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'images.unsplash.com',
-      'fdn2.gsmarena.com',
-    ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.gstatic.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.gstatic.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "loremflickr.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'fdn2.gsmarena.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "fdn2.gsmarena.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'vectorstock.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "vectorstock.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.vectorstock.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.vectorstock.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'example.com',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "example.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "plus.unsplash.com",
-        pathname: "/**"
-      }
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
